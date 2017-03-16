c7d8905e3ddfae907cfff3bd5fb6308b11842804
Henrik <cssparta@gmail.com>
2017-03-16 12:04:24 +0100

    Endra

--- CommandCenter/CommandCenter/Game.cs
+++ CommandCenter/CommandCenter/Game.cs
@@ -22,12 +22,7 @@ namespace CommandCenter
 
         private void btnName_Click(object sender, EventArgs e)
         {
-            _Player = new Player("HEY");
-            _Player.TEST.Add(new Hest("Hesasdt", 1));
 
-            Hest one = _Player.TEST[0];
-
-            lblName.Text = one._Name;
         }
     }
 }
--- CommandCenter/Engine/Player.cs
+++ CommandCenter/Engine/Player.cs
@@ -9,12 +9,10 @@ namespace Engine
     public class Player
     {
         public string _Name { get; set; }
-        public List<Hest> TEST { get; set; }
         
         public Player(string name)
         {
             _Name = name;
-            TEST = new List<Hest>();
         }
 
     }
