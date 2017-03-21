0e25c7c6595d04039b88e8a404cfe4a22f072b0f
Xistens <cssparta@gmail.com>
2017-03-21 19:29:07 +0100

    Added monster class

---
+++ CommandCenter/Engine/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Monster : Unit
+    {
+        public Monster(uint currentHealth, uint maximumHealth) : base(currentHealth, maximumHealth)
+        {
+
+        }
+    }
+}
